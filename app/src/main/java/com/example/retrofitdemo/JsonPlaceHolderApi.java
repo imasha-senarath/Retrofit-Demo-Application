320f5f94d3febb59b8f5c403546ef45fe2924385
Imasha Senarath <imashadsenarath@gmail.com>
2022-07-11 23:50:01 +0530

    Post request with body.

--- app/src/main/java/com/example/retrofitdemo/JsonPlaceHolderApi.java
+++ app/src/main/java/com/example/retrofitdemo/JsonPlaceHolderApi.java
@@ -1,12 +1,50 @@
 package com.example.retrofitdemo;
 
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.Field;
+import retrofit2.http.FieldMap;
+import retrofit2.http.FormUrlEncoded;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+import retrofit2.http.Query;
+import retrofit2.http.QueryMap;
+import retrofit2.http.Url;
 
 public interface JsonPlaceHolderApi {
 
     @GET("posts")
-    Call<List<PostModel>> getPosts();
+    Call<List<PostModel>> getPosts(
+            @Query("userId") Integer[] userId,
+            @Query("_sort") String sort,
+            @Query("_order") String order
+    );
+
+    @GET("posts")
+    Call<List<PostModel>> getPosts(@QueryMap Map<String, String> parameters);
+
+    @GET("posts/{id}/comments")
+    Call<List<CommentModel>> getComments(@Path("id") int postId);
+
+    @GET
+    Call<List<CommentModel>> getComments(@Url String url);
+
+    @POST("posts")
+    Call<PostModel> createPost(@Body PostModel post);
+
+    @FormUrlEncoded
+    @POST("posts")
+    Call<PostModel> createPost(
+            @Field("userId") int userId,
+            @Field("title") String title,
+            @Field("body") String text
+    );
+
+    @FormUrlEncoded
+    @POST("posts")
+    Call<PostModel> createPost(@FieldMap Map<String, String> fields);
 }
--- app/src/main/java/com/example/retrofitdemo/MainActivity.java
+++ app/src/main/java/com/example/retrofitdemo/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.widget.TextView;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -16,6 +18,8 @@ import retrofit2.converter.gson.GsonConverterFactory;
 public class MainActivity extends AppCompatActivity {
 
     private TextView textViewResult;
+
+    private JsonPlaceHolderApi jsonPlaceHolderApi;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +33,20 @@ public class MainActivity extends AppCompatActivity {
                 .addConverterFactory(GsonConverterFactory.create())
                 .build();
 
-        JsonPlaceHolderApi jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
+        jsonPlaceHolderApi = retrofit.create(JsonPlaceHolderApi.class);
 
-        Call<List<PostModel>> call = jsonPlaceHolderApi.getPosts();
+        //getPosts();
+        //getComments();
+        createPost();
+    }
+
+    private void getPosts() {
+        Map<String, String> parameters = new HashMap<>();
+        parameters.put("userId", "1");
+        parameters.put("_sort", "id");
+        parameters.put("_order", "desc");
+
+        Call<List<PostModel>> call = jsonPlaceHolderApi.getPosts(parameters);
 
         call.enqueue(new Callback<List<PostModel>>() {
             @Override
@@ -57,6 +72,77 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onFailure(Call<List<PostModel>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+
+    private void getComments() {
+        Call<List<CommentModel>> call = jsonPlaceHolderApi
+                .getComments(4);
+
+        call.enqueue(new Callback<List<CommentModel>>() {
+            @Override
+            public void onResponse(Call<List<CommentModel>> call, Response<List<CommentModel>> response) {
+
+                if (!response.isSuccessful()) {
+                    textViewResult.setText("Code: " + response.code());
+                    return;
+                }
+
+                List<CommentModel> comments = response.body();
+
+                for (CommentModel comment : comments) {
+                    String content = "";
+                    content += "ID: " + comment.getId() + "\n";
+                    content += "Post ID: " + comment.getPostId() + "\n";
+                    content += "Name: " + comment.getName() + "\n";
+                    content += "Email: " + comment.getEmail() + "\n";
+                    content += "Text: " + comment.getText() + "\n\n";
+
+                    textViewResult.append(content);
+                }
+            }
+
+            @Override
+            public void onFailure(Call<List<CommentModel>> call, Throwable t) {
+                textViewResult.setText(t.getMessage());
+            }
+        });
+    }
+
+    private void createPost() {
+        PostModel post = new PostModel(23, "New Title", "New Text");
+
+        Map<String, String> fields = new HashMap<>();
+        fields.put("userId", "25");
+        fields.put("title", "New Title");
+
+        Call<PostModel> call = jsonPlaceHolderApi.createPost(post);
+
+        call.enqueue(new Callback<PostModel>() {
+            @Override
+            public void onResponse(Call<PostModel> call, Response<PostModel> response) {
+
+                if (!response.isSuccessful()) {
+                    textViewResult.setText("Code: " + response.code());
+                    return;
+                }
+
+                PostModel postResponse = response.body();
+
+                String content = "";
+                content += "Code: " + response.code() + "\n";
+                content += "ID: " + postResponse.getId() + "\n";
+                content += "User ID: " + postResponse.getUserId() + "\n";
+                content += "Title: " + postResponse.getTitle() + "\n";
+                content += "Text: " + postResponse.getText() + "\n\n";
+
+                textViewResult.setText(content);
+            }
+
+            @Override
+            public void onFailure(Call<PostModel> call, Throwable t) {
                 textViewResult.setText(t.getMessage());
             }
         });
